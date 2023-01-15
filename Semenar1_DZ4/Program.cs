68e3a8dc97f28ee241a13d91f293e7bd0141d064
Roman <rangerdgedi@mail.ru>
2023-01-15 17:55:56 +0300

    add DZ4 Semenar1

---
+++ Semenar1_DZ4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите первое число: ");
+int n = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int m = int.Parse(Console.ReadLine());
+    for (int i = n; i <= m; i++)
+        {
+    if (i % 2 == 0) 
+        Console.Write(i + " ");
+        }
