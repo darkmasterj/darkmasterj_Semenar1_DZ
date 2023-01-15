e197fbc5b772f12f4803705be3c5fa1e223c8e54
Roman <rangerdgedi@mail.ru>
2023-01-15 17:45:09 +0300

    add DZ2 Semenar1

---
+++ Semenar1_DZ1/Semenar1_DZ2/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число:");
+int c = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Math.Max(a, Math.Max(b, c)));
