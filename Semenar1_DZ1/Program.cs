d328b63bf8edc7cb6fa59597f7d54265c6278fa6
Roman <rangerdgedi@mail.ru>
2023-01-15 17:22:57 +0300

    add DZ1 Semenar1

---
+++ Semenar1_DZ1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(a > b ? "Первое число больше второго" : "Второе число больше первого");
