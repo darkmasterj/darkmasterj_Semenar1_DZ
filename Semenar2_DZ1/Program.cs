4465cb7b2211b5f6ad806339217100615602dbe3
Roman <rangerdgedi@mail.ru>
2023-01-18 15:51:58 +0300

    Добавил ДЗ1 к семенару2 с интересной фичей на команду выход

---
+++ Semenar2_DZ1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите трехзначное число.\nВыход из программы команда exit.");
+    while(true)
+    {
+        string input = Console.ReadLine();
+        if(!input.Equals("exit"))
+            Console.WriteLine("{0}->{1}",input, input[1]);
+        else
+            break;
+    }
+    
