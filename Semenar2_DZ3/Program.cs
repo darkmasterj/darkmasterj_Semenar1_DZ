d4342d2be4e444c1ca1e6b61a60a7b45b260a7f0
Roman <rangerdgedi@mail.ru>
2023-01-18 16:21:44 +0300

    Добавил ДЗ 3 к семенару 2

---
+++ Semenar2_DZ3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write(" Давай узнаем выходной день или нет  "); 
+            while (true) 
+            { 
+                Console.Write("\n Введите номер дня недели  "); 
+                int num = int.Parse(Console.ReadLine()); 
+                if (num >= 1 && num <= 5) 
+                { 
+                    Console.Write(" Нет, это не выходной"); 
+                } 
+ 
+                else if (num >= 6 && num <= 7) 
+                { 
+                    Console.Write(" Да, это выходной"); 
+                } 
+                else Console.Write(" В неделе всего 7 дней! Давай по новой!"); 
+            } 
