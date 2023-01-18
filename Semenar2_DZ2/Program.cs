925dd0ce6ac6d489ee2261b9a675d6b4905fa34b
Roman <rangerdgedi@mail.ru>
2023-01-18 16:10:38 +0300

    Добавил ДЗ2 к семенару2

---
+++ Semenar2_DZ2/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int n = int.Parse(Console.ReadLine());
+int k = (int)Math.Log10(n)-2;
+Console.WriteLine(k < 0 ? "Нет третей цифры" : (n % (int)Math.Pow(10, k + 1) / (int)Math.Pow(10, k)).ToString());
+Console.ReadLine();
