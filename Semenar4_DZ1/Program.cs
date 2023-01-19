b7857d3b53cf1c11bd8b3d1efbb6647ed5013258
Roman <rangerdgedi@mail.ru>
2023-01-19 14:27:00 +0300

    Добавил ДЗ 1 к семенару 4

---
+++ Semenar4_DZ1/Program.cs
@@ -0,0 +1,31 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+using System;
+namespace Example
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите любое число: ");
+            string number = Console.ReadLine();
+            ushort sum = 0;
+            bool point = true;
+            char symbol;
+            for (int i = 0; i < number.Length; ++i)
+            {
+                symbol = number[i];
+                if (i == 0 && symbol == '-' || symbol == '+') continue;
+                if (point && symbol == '.')
+                {
+                    point = false;
+                    continue;
+                }
+                if (Char.IsDigit(symbol)) sum += Convert.ToUInt16(symbol.ToString());
+                else break;
+            }
+            Console.WriteLine("Сумма цифр в числе= " + sum);
+            Console.ReadKey();
+        }
+    }
+}
