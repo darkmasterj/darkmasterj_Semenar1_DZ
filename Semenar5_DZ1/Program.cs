ab1d83d6c4eca1368c00dc1b6ad0d78aab2d219b
Roman <rangerdgedi@mail.ru>
2023-01-19 18:25:09 +0300

    Добавил ДЗ к семенару 5

---
+++ Semenar5_DZ1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Найдите произведение пар чисел в одномерном массиве.
+// Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+// Результат запишите в новом массиве
+
+using System; 
+ 
+namespace func 
+{ 
+  static class func 
+  { 
+    static void Main() 
+    { 
+      Console.WriteLine("Размер массива: "); 
+      int n; 
+      n = Convert.ToInt32(Console.ReadLine()); 
+      int[] ar = new int[n]; 
+      var rand = new Random(); 
+       
+      for(int i = 0; i < ar.Length; i++) 
+        ar[i] = rand.Next(-50, 50); 
+      Console.Write("Значения элементов массива: "); 
+      foreach(var item in ar) 
+        Console.Write("{0} ", item); 
+      Console.WriteLine(); 
+      for(int i = 0; i < ar.Length / 2; i++) 
+        Console.WriteLine("элемент {0} * элемент {1}, {2} * {3}  = {4}", i, ar.Length - i - 1, ar[i], ar[ar.Length - i - 1], ar[i] * ar[ar.Length - i - 1]); 
+      return; 
+    } 
+  } 
+}
